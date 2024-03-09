e0d07753a314b9186af499c3482ec17415ef5b8d
Anwar_Hossain <ranakrphone@gmail.com>
2024-03-10 00:31:04 +0600

    slide-show web

---
+++ script.js
@@ -0,0 +1,17 @@
+const slideshowElements = document.querySelectorAll(".slideshow-element");
+console.log(slideshowElements);
+
+let countElements = 1;
+
+setInterval(() => {
+  countElements++;
+  let currentSlide = document.querySelector(".current");
+
+  currentSlide.classList.remove("current");
+  if (countElements > slideshowElements.length) {
+    slideshowElements[0].classList.add("current");
+    countElements = 1;
+  } else {
+    currentSlide.nextElementSibling.classList.add("current");
+  }
+}, 2000);
